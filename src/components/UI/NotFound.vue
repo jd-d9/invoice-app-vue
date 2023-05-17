76f1372a34b34d93f9017c6f498a2f8207c3bbda
jd <d9ithub@gmail.com>
2023-05-17 18:32:25 +0530

    edit not-found page

--- src/components/UI/NotFound.vue
+++ src/components/UI/NotFound.vue
@@ -1,14 +1,13 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-12">
+            <div class="col-5 mx-auto">
                 <div class="not-found">
                     <div class="detailing">
                         <h1>404</h1>
-                        <div>
-                            <h5>Page Not Found.</h5>
-                            <p class="mb-0">May you looking for <router-link to="/" class="text-warning">This</router-link> Page?</p>
-                        </div>
+                        <h5>OOPS! PAGE NOT FOUND</h5>
+                        <p class="mb-5">Sorry, the page you're looking for dosen't exist. It might have been removed or renamed.</p>
+                        <router-link to="/" class="dashboard-button">Go to Dashboard</router-link>
                     </div>
                 </div>
             </div>
@@ -24,17 +23,24 @@
         place-items: center;
     }
     .detailing {
-        display: flex;
-        justify-content: center;
-        align-items: center;
+        text-align: center;
         box-shadow: 0 0 4px rgb(183, 183, 183);
         gap: 20px;
         border-radius: 5px;
-        padding: 30px 40px;
+        padding: 20px 40px 50px;
     }
     h1 {
-        font-size: 70px;
+        font-size: 120px;
+        font-weight: bolder;
         margin-bottom: 0;
         transform: translateY(-10px);
+    }
+    .dashboard-button {
+        text-decoration: none;
+        background-color: #ffa600;
+        padding: 8px 50px;
+        border-radius: 50px;
+        color: white;
+        border: 1px solid transparent;
     }
 </style>
